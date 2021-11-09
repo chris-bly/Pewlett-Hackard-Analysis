6a0891322f29d33ef349a797558f8fa269a8057f
chris-bly <chrisalanbly@gmail.com>
2021-11-08 20:29:42 -0500

    Adding Challenge code for parts 1 and 2 along with output CSV files

---
+++ Analysis_Projects_Folder/Pewlett_Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,48 @@
+-- Challenge 1 code
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--Retrieve number of employees by their most recent job title
+SELECT COUNT (ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- Challenge 2 code
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY (e.emp_no);
